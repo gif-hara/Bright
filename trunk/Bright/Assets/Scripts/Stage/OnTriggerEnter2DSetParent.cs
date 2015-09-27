5cafcf1a5541627bd5d9cef455ed126aa78f46e1
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2015-09-27 14:32:42 +0900

    侵入したチャンクの子供になるよう実装

---
+++ trunk/Bright/Assets/Scripts/Stage/OnTriggerEnter2DSetParent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Bright
+{
+	/// <summary>
+	/// .
+	/// </summary>
+	public class OnTriggerEnter2DSetParent : MonoBehaviour
+	{
+		[SerializeField]
+		private Transform child;
+
+		void OnTriggerEnter2D(Collider2D other)
+		{
+			this.child.parent = other.transform;
+		}
+	}
+}
