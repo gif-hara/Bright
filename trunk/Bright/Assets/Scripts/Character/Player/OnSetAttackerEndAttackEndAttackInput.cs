780b5b23c48524ee25d20961345eba4776716ffe
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2015-09-28 23:30:07 +0900

    レーザーの実装
    ボムの生成方法の修正

---
+++ trunk/Bright/Assets/Scripts/Character/Player/OnSetAttackerEndAttackEndAttackInput.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Bright
+{
+	/// <summary>
+	/// OnSetAttackerイベントで攻撃終了処理を行うコンポーネント.
+	/// </summary>
+	public class OnSetAttackerEndAttackEndAttackInput : MonoBehaviour, IReceiveSetAttacker
+	{
+		public void OnSetAttacker(GameObject attacker)
+		{
+			StartCoroutine(this.EndAttackCoroutine(attacker));
+		}
+
+		IEnumerator EndAttackCoroutine(GameObject attacker)
+		{
+			while(Bright.Input.AttackButton)
+			{
+				yield return 0;
+			}
+
+			ExecuteEvents.Execute<IReceiveEndAttack>(attacker, null, (handler, eventData) => handler.OnEndAttack());
+			Debug.Log("OK");
+		}
+	}
+}
---
+++ trunk/Bright/Assets/Scripts/Component/IntervalToggleActive.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Bright
+{
+	/// <summary>
+	/// 間隔でゲームオブジェクトのアクティブフラグをトグルするコンポーネント.
+	/// </summary>
+	public class IntervalToggleActive : MonoBehaviour
+	{
+		[SerializeField]
+		private GameObject target;
+
+		[SerializeField]
+		private float interval;
+
+		private float duration = 0.0f;
+
+		void Update()
+		{
+			if(this.duration < this.interval)
+			{
+				this.duration += Time.deltaTime;
+				return;
+			}
+
+			this.duration = 0.0f;
+			this.target.SetActive(!this.target.activeSelf);
+		}
+	}
+}
