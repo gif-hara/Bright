7326bf82186cc43fe666e169c84061933f63653b
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2015-09-29 23:50:39 +0900

    ファンネルの実装中

---
+++ trunk/Bright/Assets/Scripts/Component/CreatePrefab/CanCreatePrefabNotNullHoldObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Bright
+{
+	/// <summary>
+	/// .
+	/// </summary>
+	public class CanCreatePrefabNotNullHoldObject : MonoBehaviour
+	{
+	}
+}
