45207c342b05cc48fd3c66b8acb8b38f1af8bb9a
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2015-09-30 01:51:03 +0900

    マインの実装

---
+++ trunk/Bright/Assets/Scripts/Character/Common/GiveDamage/OnGiveDamageCreatePrefab.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Bright
+{
+	/// <summary>
+	/// OnGiveDamageイベント時にプレハブを生成するコンポーネント.
+	/// </summary>
+	public class OnGiveDamageCreatePrefab : MonoBehaviour, IReceiveGiveDamage
+	{
+		[SerializeField]
+		private CreatePrefab creator;
+
+		public void OnGiveDamage(GameObject giveObject, Collider2D takeObject)
+		{
+			this.creator.Create(this.gameObject, this.gameObject);
+		}
+	}
+}
