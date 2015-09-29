5035dea5bda59c3e6b9686c93171c7e68ee916d3
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2015-09-29 23:10:32 +0900

    DelayTimerクラスの実装

---
+++ trunk/Bright/Assets/Scripts/Component/CreatePrefab/IntervalCreatePrefab.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Bright
+{
+	/// <summary>
+	/// 一定間隔でプレハブを生成するコンポーネント.
+	/// </summary>
+	public class IntervalCreatePrefab : MonoBehaviour
+	{
+		[SerializeField]
+		private CreatePrefab creator;
+
+		[SerializeField]
+		private DelayTimer interval;
+
+		void Update()
+		{
+			this.interval.Update(Time.deltaTime);
+			if(!this.interval.Complete)
+			{
+				return;
+			}
+
+			this.creator.Create(this.gameObject);
+			this.interval.Reset();
+		}
+	}
+}
---
+++ trunk/Bright/Assets/Scripts/Framework/DelayTimer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Bright
+{
+	/// <summary>
+	/// 遅延タイマークラス.
+	/// </summary>
+	[System.Serializable]
+	public class DelayTimer
+	{
+		[SerializeField]
+		private float delay;
+
+		private float duration = 0.0f;
+
+		public void Update(float t)
+		{
+			this.duration += t;
+		}
+
+		public void Reset()
+		{
+			this.duration = 0.0f;
+		}
+
+		public bool Complete
+		{
+			get
+			{
+				return this.delay < this.duration;
+			}
+		}
+	}
+}
