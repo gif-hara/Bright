949dc75e9260a36a9e5ac74c9cb91d6191050940
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2015-09-29 23:01:07 +0900

    ファンネルの実装中

--- trunk/Bright/Assets/Scripts/Framework/GameObjectExtensions.cs
+++ trunk/Bright/Assets/Scripts/Framework/GameObjectExtensions.cs
@@ -15,5 +15,25 @@ namespace Bright
 		{
 			return self.CompareTag("Player");
 		}
+
+		public static GameObject FindGameObjectWithTag(this GameObject self, string tag, float radius)
+		{
+			var findObjects = GameObject.FindGameObjectsWithTag(tag);
+			var origin = self.transform.position;
+			GameObject result = null;
+			float min = radius;
+			for(int i=0, imax=findObjects.Length; i<imax; i++)
+			{
+				var obj = findObjects[i];
+				var distance = Vector3.Distance(origin, obj.transform.position);
+				if(min > distance)
+				{
+					min = distance;
+					result = obj;
+				}
+			}
+
+			return result;
+		}
 	}
 }
