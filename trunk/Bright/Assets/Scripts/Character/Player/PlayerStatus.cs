50a937690ea5132b07181f48e526832b66df35d9
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2015-10-02 00:25:18 +0900

    PlayerStatusクラスの実装

---
+++ trunk/Bright/Assets/Scripts/Character/Player/PlayerStatus.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Bright
+{
+	/// <summary>
+	/// プレイヤーステータスクラス.
+	/// </summary>
+	public class PlayerStatus
+	{
+		public Wallet Wallet{ private set; get; }
+
+		public PlayerStatus()
+		{
+			this.Wallet = new Wallet();
+		}
+	}
+}
---
+++ trunk/Bright/Assets/Scripts/Character/Player/Wallet.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Bright
+{
+	/// <summary>
+	/// 財布クラス.
+	/// </summary>
+	public class Wallet
+	{
+		public int Money{ private set; get; }
+
+		public Wallet()
+		{
+			this.Money = 0;
+		}
+
+		public void Add(int value)
+		{
+			this.Money += value;
+		}
+
+		public bool IsEnough(int value)
+		{
+			return this.Money >= value;
+		}
+	}
+}
