38ed9b803b2956743cdc6775e7e99449ddf225ae
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2015-10-02 00:25:26 +0900

    ForLoopクラスの実装中

---
+++ trunk/Bright/Assets/Scripts/Framework/ForLoop.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Bright
+{
+	/// <summary>
+	/// 指定した回数分イベントを実行するクラス.
+	/// </summary>
+	[System.Serializable]
+	public class ForLoop
+	{
+		[SerializeField]
+		private int min;
+
+		[SerializeField]
+		private int max;
+
+		[SerializeField]
+		private UnityEvent functor;
+
+		public void Execute()
+		{
+			for(int i=min; i<max; i++)
+			{
+				functor.Invoke();
+			}
+		}
+	}
+}
