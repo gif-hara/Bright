084f22c1c23aa1f3ab8bf121b16bbfd7568f219f
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2015-10-12 22:28:16 +0900

    ゴブリンのAIを実装
    プレイヤーを攻撃出来るよう実装

---
+++ trunk/Bright/Assets/Scripts/State/OnActiveStateOtherEvent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Bright
+{
+	/// <summary>
+	/// .
+	/// </summary>
+	public class OnActiveStateOtherEvent : MonoBehaviour, IReceiveActiveState
+	{
+		[SerializeField]
+		private UnityEvent otherEvent;
+
+		public void OnActiveState()
+		{
+			this.otherEvent.Invoke();
+		}
+	}
+}
