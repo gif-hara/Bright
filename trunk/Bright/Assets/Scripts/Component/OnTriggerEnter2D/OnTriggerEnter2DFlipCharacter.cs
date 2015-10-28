d410afabecb9f4250e6c2b36871217d4badec275
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2015-10-29 00:10:53 +0900

    弓ゴブリンの後ろにプレイヤーが回ったら振り向くよう実装

---
+++ trunk/Bright/Assets/Scripts/Component/OnTriggerEnter2D/OnTriggerEnter2DFlipCharacter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Bright
+{
+	/// <summary>
+	/// .
+	/// </summary>
+	public class OnTriggerEnter2DFlipCharacter : MonoBehaviour
+	{
+		[SerializeField]
+		private LayerMask ignoreLayer;
+		
+		[SerializeField]
+		private PlatformerCharacter2D character;
+
+		void OnTriggerEnter2D(Collider2D other)
+		{
+			if(this.ignoreLayer.IsIncluded(other.gameObject))
+			{
+				return;
+			}
+
+			this.character.Flip();
+		}
+	}
+}
